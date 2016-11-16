14a1ea07264472ba8e94abb197ed13933f33414b
Nikhil Kansal <nkansal96@gmail.com>
2016-11-15 16:54:02 -0800

    changed pg connection to object

--- app/ghost/config.js
+++ app/ghost/config.js
@@ -17,7 +17,15 @@ config = {
         mail: {},
         database: {
             client: 'pg',
-			connection: process.env.DATABASE_URL,
+			connection: {
+				host: process.env.DB_HOST,
+				port: process.env.DB_PORT,
+				user: process.env.DB_USER,
+				password: process.env.DB_PASS,
+				database: process.env.DB_NAME,
+				ssl: true
+			},
+//			connection: process.env.DATABASE_URL,
             debug: true
         },
 
