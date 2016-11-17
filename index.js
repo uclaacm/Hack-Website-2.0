e4e02c7708347cf024bf232ad5fa9ea9e81d073c
Nikhil Kansal <nkansal96@gmail.com>
2016-11-17 02:30:52 -0800

    enabled ghost after merge

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 5000;
 let server = express();
 
 // start the ghost server
-// app.ghost(server);
 server.use(express.static('www/public'));
 server.set('view engine', 'hbs');
 server.use('/signin', app.signIn.Router);
+app.ghost(server);
 
 server.listen(port, () => {
 	console.log("started server on port", port);
