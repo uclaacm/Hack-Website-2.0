33841a92aa3bcc7e9e29cd71e610d2977bf6f5e5
Nikhil Kansal <nkansal96@gmail.com>
2016-11-20 15:18:19 -0800

    added production db

--- app/config/index.js
+++ app/config/index.js
@@ -4,19 +4,13 @@ let config = { database: {} };
 if (env === "production") {
     config.host = process.env.host;
     config.sessionSecret = process.env.sessionSecret;
-    // config.database.host;
-    // config.database.user;
-    // config.database.port;
-    // config.database.options;
+    config.database.uri = "mongodb://acm-hack:bXm-BT4-gfb-FaW@ds159237.mlab.com:59237/heroku_554lzv52";
 } else {
     config.host = "http://localhost:5000";
     config.sessionSecret = "77ea260f6918c0d8c3b6c35514d3b1a4fc69f01adbf7d2412611de97c3f0f2dc";
-    config.database.host = "127.0.0.1";
-    config.database.port = "27017";
-    config.database.database = "acm-hack-db";
+    config.database.uri = "mongodb://127.0.0.1:27017/acm-hack-db";
 }
 
-config.database.uri = "mongodb://" + config.database.host + ":" + config.database.port + "/" + config.database.database;
 config.isProduction = env === "production";
 config.isDevelopment = !config.isProduction;
 
