e056da656a0085af69a7e31967833fede742e1a5
Nikhil Kansal <nkansal96@gmail.com>
2016-11-20 02:17:42 -0800

    updated signin to use sessions, properly named templates

--- app/signin/index.js
+++ app/signin/index.js
@@ -1,94 +1,114 @@
 const querystring = require('querystring');
 const express = require('express');
-let db = require('../db');
 let router = express.Router();
+let db = require('../db');
 
 router.param('eventCode', (req, res, next, eventCode) => {
+    if (!req.session.user)
+        req.session.user = {};
     db.Event.findByCode(eventCode, (err, event) => {
         if (err)
             return res.render('signin/error', { error: "The event '" + eventCode + "' was not found" });
-        req.user = { event };
+        req.session.user.event = event;
         next();
     });
 });
 
+router.get('/', (req, res) => {
+    db.Event.findAllInProgress((err, events) => {
+        res.render('signin/events', { events: err ? null : events });
+    });
+});
+
 router.get('/:eventCode', (req, res) => {
-    return res.render('signin/index', { event: req.user.event });
+    return res.render('signin/signin', { event: req.session.user.event });
+});
+
+router.get('/:eventCode/reset', (req, res) => {
+    let eventCode = req.session.user.event.code;
+    req.session.user = null;
+    res.redirect(req.baseUrl + '/' + eventCode);
 });
 
 router.post('/:eventCode/signin', (req, res) => {
     let uid = (req.body.uid || "").replace(/[^\d]/g, ""); // remove non-digits
     if (!uid || uid.length !== 9)
-        return res.redirect(req.baseUrl + '/' + req.user.event.code);
+        return res.redirect(req.baseUrl + '/' + req.session.user.event.code);
 
-    db.User.findByUID(uid, (err, user) => {
+    db.Attendee.findByUID(uid, (err, attendee) => {
+        req.session.user.uid = uid;
+        req.session.user.attendee = attendee;
         if (err)
-            return res.redirect(req.baseUrl + '/' + req.user.event.code + '/moreinfo/' + uid);
+            return res.redirect(req.baseUrl + '/' + req.session.user.event.code + '/moreinfo');
         
-        user.addEvent(req.user.event);
-        res.redirect(req.baseUrl + '/' + req.user.event.code + '/success/' + uid);
+        attendee.addEvent(req.session.user.event);
+        res.redirect(req.baseUrl + '/' + req.session.user.event.code + '/success');
     });
 });
 
-router.get('/:eventCode/moreinfo/:uid', (req, res) => {
-    let uid = (req.params.uid || "").replace(/[^\d]/g, ""); // remove non-digits
-    if (!uid || uid.length !== 9)
-        return res.redirect(req.baseUrl + '/' + req.user.event.code);
-
-    let uriData = req.query.d || null;
-    if (uriData)
-        uriData = querystring.parse(Buffer.from(uriData, 'base64').toString('ascii'));
-    
-    db.User.findByUID(uid, (err, user) => {
-        if (err)
-            return res.render('signin/moreinfo', { event: req.user.event, uid , user: uriData });
+router.get('/:eventCode/moreinfo', (req, res) => {
+    if (!req.session.user || !req.session.user.uid)
+        return res.redirect(req.baseUrl + '/' + req.session.user.event.code);
+ 
+    db.Attendee.findByUID(req.session.user.uid, (err, attendee) => {
+        req.session.user.attendee = attendee;
+        if (err) {
+            return res.render('signin/moreinfo', {
+                uid: req.session.user.uid,
+                event: req.session.user.event,
+                attendee: req.session.user.errorData,
+                error: {
+                    error: req.session.user.error,
+                    message: 'Some of the information you submitted was either invalid or incomplete.'
+                }
+            });
+        }
         
-        user.addEvent(req.user.event);
-        res.redirect(req.baseUrl + '/' + req.user.event.code + '/success/' + uid);
+        attendee.addEvent(req.session.user.event);
+        res.redirect(req.baseUrl + '/' + req.session.user.event.code + '/success');
     });
 });
 
 router.post('/:eventCode/adduser', (req, res) => {
-    let newUserData = {
-        firstName: req.body.firstName || "",
-        lastName: req.body.lastName || "",
-        email: req.body.email || "",
-        major: req.body.major || "",
-        year: req.body.year || "",
-        uid: req.body.uid || ""
+    let newAttendeeData = {
+        firstName: (req.body.firstName || "").trim(),
+        lastName: (req.body.lastName || "").trim(),
+        email: (req.body.email || "").trim(),
+        major: (req.body.major || "").trim(),
+        year: (req.body.year || "").trim(),
+        uid: (req.body.uid || "").trim()
     };
 
-    
-    db.User.findByUID(newUserData.uid, (err, user) => {
-        if (!err && user) {
-            user.addEvent(req.user.event, (err, user, numAffected) => {
-                res.redirect(req.baseUrl + '/' + req.user.event.cod + '/success/' + user.uid);
+    db.Attendee.findByUID(newAttendeeData.uid, (err, attendee) => {
+        if (!err && attendee) {
+            attendee.addEvent(req.session.user.event, (err, attendee, numAffected) => {
+                req.session.user.attendee = attendee;
+                res.redirect(req.baseUrl + '/' + req.session.user.event.code + '/success');
             });
         } else {
-            let newUser = new db.User(newUserData);
-            newUser.addEvent(req.user.event, (err, user, numAffected) => {
-                if (!err && user)
-                    return res.redirect(req.baseUrl + '/' + req.user.event.code);
+            let newAttendee = new db.Attendee(newAttendeeData);
+            newAttendee.addEvent(req.session.user.event, (err, attendee, numAffected) => {
+                req.session.user.attendee = attendee;
+                if (!err && attendee)
+                    return res.redirect(req.baseUrl + '/' + req.session.user.event.code + '/success');
                 
-                const uriData = Buffer.from(querystring.stringify(newUserData)).toString('base64');
-                res.redirect(req.baseUrl + '/' + req.user.event.code + '/moreinfo/' + newUserData.uid + '?d=' + uriData);
+                req.session.user.error = true;
+                req.session.user.errorData = newAttendeeData;
+                res.redirect(req.baseUrl + '/' + req.session.user.event.code + '/moreinfo');
             });
         }
     });
 });
 
-router.get('/:eventCode/success/:uid', (req, res) => {
-    let uid = (req.params.uid || "").replace(/[^\d]/g, ""); // remove non-digits
-    if (!uid || uid.length !== 9)
-        return res.redirect(req.baseUrl + '/' + req.user.event.code);
+router.get('/:eventCode/success', (req, res) => {
+    if (!req.session.user || !req.session.user.attendee)
+        return res.redirect(req.baseUrl + '/' + req.session.user.event.code);
 
-    db.User.findByUID(uid, (err, user) => {
-        if (err || !user.attendedEvent(req.user.event))
-            return res.redirect(req.baseUrl + '/' + req.user.event.code);
-        res.render('signin/signedin', { event: req.user.event, user });
+    res.render('signin/success', { 
+        event: req.session.user.event,
+        attendee: req.session.user.attendee
     });
+    req.session.user = null;
 });
 
-module.exports = {
-    Router: router
-};
+module.exports = { router };
