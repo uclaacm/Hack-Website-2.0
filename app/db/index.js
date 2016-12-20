ace4f8403a4bbd4d22d0b351f9d5a364a748d43e
Nikhil Kansal <nkansal96@gmail.com>
2016-12-19 18:25:43 -0800

    created mongoose model of score schema

--- app/db/index.js
+++ app/db/index.js
@@ -4,7 +4,8 @@ const config = require('../config');
 mongoose.connect(config.database.uri);
 
 let Project = mongoose.model('Project', require('./schema/project'));
+let Score = mongoose.model('Score', require('./schema/score'));
 let Event = mongoose.model('Event', require('./schema/event'));
 let User = mongoose.model('User', require('./schema/user'));
 
-module.exports = { Project, Event, User };
+module.exports = { Project, Score, Event, User };
