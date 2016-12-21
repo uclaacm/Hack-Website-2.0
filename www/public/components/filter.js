331db1d98dd2f53d9cde1e843e4483b4e15e112d
Yvonne Chen <yc.yvonnechen@yahoo.com>
2016-12-22 00:34:18 +0800

    updated filter functionality: multiple checks, clear and select all

--- www/public/components/filter.js
+++ www/public/components/filter.js
@@ -8,24 +8,68 @@ class Filter extends Component{
 	constructor(props){
 		super(props);
 
+		this.state = { selectedCategories: []};
+
 		//binding functions
+		this.setAllCheckBoxes = this.setAllCheckBoxes.bind(this);
+		this.setStateAndCallAction = this.setStateAndCallAction.bind(this);
+		this.onCheckBoxToggle = this.onCheckBoxToggle.bind(this);
 		this.renderList = this.renderList.bind(this);
-		this.onRadioSelect = this.onRadioSelect.bind(this);
 	}
 
-	onRadioSelect(e){
-		const selectedCategory = e.target.value;
+	//sets default state to be all checked
+	componentDidMount(){
+		this.toggleBoxes(true);
+	}
 
-		//call action creator with newly selected events
-		this.props.filterEvents(
-			this.props.events.filter(event => event.category == selectedCategory)
-		);
+	setAllCheckBoxes(toggle)
+	{
+		[...document.forms['filter'].elements].forEach(element => {
+			if(element.type == 'checkbox')
+				element.checked = toggle;
+		});
+	}
+
+	setStateAndCallAction(newCategories){
+		this.setState({
+			selectedCategories : newCategories
+		}, () => {
+			this.props.filterEvents(
+				this.props.events.filter(event => this.state.selectedCategories.includes(event.category))
+			);
+		});
+	}
+
+	//false unchecks all boxes, true checks all boxes
+	toggleBoxes(toggle){
+		this.setAllCheckBoxes(toggle);
+		if(!toggle)
+			this.setStateAndCallAction([]);
+		else
+			this.setStateAndCallAction(this.props.categories);
+	}
+
+	onCheckBoxToggle(e){		
+		const selectedCategory = e.target.value;
+		const stateCats = this.state.selectedCategories;
+		const index = this.state.selectedCategories.indexOf(selectedCategory);
+		
+		//updating state to reflect checked checkboxes
+		let newCategories;
+		if(index == -1)
+			newCategories = [...stateCats, selectedCategory];
+		else
+			newCategories = [...stateCats.slice(0, index),
+							 ...stateCats.slice(index+1)];
+
+		//passing in component state to action creator
+		this.setStateAndCallAction(newCategories)
 	}
 
 	renderList(category){
 		return (
 			<li key={category}>
-				<input type="radio" name="category" value={category} onChange={this.onRadioSelect}/>
+				<input type="checkbox" value={category} onChange={this.onCheckBoxToggle}/>
 				<label htmlFor={category}>{category}</label>
 			</li>
 		);
@@ -33,9 +77,23 @@ class Filter extends Component{
 
 	render(){
 		return(
-			<form>
+			<form name="filter">
 				<ul>
 					{this.props.categories.map(this.renderList)}
+					<li key="clear">
+						<button onClick={(e) =>{
+							this.toggleBoxes(false);
+							e.preventDefault();
+						}}>
+							clear
+						</button>
+						<button onClick={(e) => {
+							this.toggleBoxes(true);
+							e.preventDefault();
+						}}>
+							all
+						</button>
+					</li>
 				</ul>
 			</form>
 		);
