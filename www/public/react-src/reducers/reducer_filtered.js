70fe97f47a1981c0b4357460e8db50997a420bdb
Yvonne Chen <yc.yvonnechen@yahoo.com>
2016-12-23 02:17:30 +0800

    sort events on page by date

--- www/public/react-src/reducers/reducer_filtered.js
+++ www/public/react-src/reducers/reducer_filtered.js
@@ -4,7 +4,11 @@ export default (state = [], action) => {
 
 	switch(action.type){
 		case FILTER_EVENTS:
-			return action.payload;
+			return action.payload.sort((a,b) => {
+				const first = new Date(a.date.start);
+				const second = new Date(b.date.start);
+				return first < second ? 1 : -1;
+			});
 		default:
 			return state;
 	}
