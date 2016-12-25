536c484e7a5a255c3bff4d10cc2ed3c26693e445
Nikhil Kansal <nkansal96@gmail.com>
2016-12-25 13:28:08 -0800

    schema changes, helper functions added to hack school schemas

--- app/db/schema/project.js
+++ app/db/schema/project.js
@@ -1,13 +1,34 @@
+const uuid = require('node-uuid');
+const _ = require('underscore');
 let Schema = require('mongoose').Schema;
 let ObjectId = Schema.ObjectId;
 
 let Project = new Schema({
+	id: {
+		type: String,
+		required: true,
+		unique: true,
+		default: () => uuid.v4()
+	},
 	number: { type: Number, required: true, unique: true },
+	points: { type: Number, required: true },
 	name: { type: String, required: true },
-	desc: { type: String },
-	image: { type: String },
-	shortDesc: { type: String },
-	submissionLink: { type: String }
+	desc: { type: String, required: true },
+	image: { type: String, required: true },
+	videoLink: { type: String },
+	slidesLink: { type: String, required: true  },
+	blogPostLink: { type: String },
+	submissionLink: { type: String },
 });
+
+Project.statics.getAll = function(callback) {
+	this.find({}, (err, projects) => {
+		return callback(err, projects);
+	});
+};
+
+Project.methods.getPublic = function() {
+	return _.pick(this, ['id','number','points','name','desc','image','videoLink','slidesLink','blogPostLink','submissionLink']);
+};
 
 module.exports = Project;
--- app/db/schema/team.js
+++ app/db/schema/team.js
@@ -2,7 +2,7 @@ const uuid = require('node-uuid');
 let Schema = require('mongoose').Schema;
 let ObjectId = Schema.ObjectId;
 
-let Score = require('./score');
+let User = require('./user');
 let Team = new Schema({
     id: {
 		type: String,
@@ -11,22 +11,36 @@ let Team = new Schema({
 		default: () => uuid.v4() 
 	},
 	name: { type: String, required: true },
-	memberIds: { type: [String], required: true },
-	scores: { type: [Score] }
+	members: { type: [User], required: true },
+	scores: { type: [{ projectNumber: Number, score: Number }] }
 });
 
+Team.statics.getAll = function(callback) {
+	this.find({}, (err, teams) => {
+		return callback(err, teams);
+	});
+};
+
 Team.statics.findById = function(id, callback) {
-	this.findOne({ id }, (err, user) => {
-		callback(err, user);
+	this.findOne({ id }, (err, team) => {
+		return callback(err, team);
+	});
+};
+
+Team.statics.findByName = function(name, callback) {
+	this.findOne({ name }, (err, team) => {
+		return callback(err, team);
 	});
 };
 
 Team.methods.getPublic = function() {
-	/**
-	 * TODO: implement public team object
-	 * Return a publicized version of the team object
-	 * It should have the total score, individual project scores, list of public member profiles, id, and name
-	 */
+	return {
+		id: this.id,
+		name: this.name,
+		scores: this.scores,
+		members: this.members.map(member => member.getPublic()),
+		totalScore: this.scores.reduce((a,b) => a.score + b.score, 0)
+	};
 };
 
 module.exports = Team;
