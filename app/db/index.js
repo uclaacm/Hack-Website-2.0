b948e2208c83c3a3d45eac39f664607580e02aec
Nikhil Kansal <nkansal96@gmail.com>
2016-12-25 13:27:51 -0800

    deleted score schema

--- app/db/index.js
+++ app/db/index.js
@@ -7,9 +7,9 @@ mongoose.connect(config.database.uri);
 
 let ShowcaseProject = mongoose.model('ShowcaseProject', require('./schema/showcaseProject'));
 let Project = mongoose.model('Project', require('./schema/project'));
-let Score = mongoose.model('Score', require('./schema/score'));
 let Event = mongoose.model('Event', require('./schema/event'));
-let User = mongoose.model('User', require('./schema/user'));
 let Email = mongoose.model('Email', require('./schema/email'));
+let User = mongoose.model('User', require('./schema/user'));
+let Team = mongoose.model('Team', require('./schema/team'));
 
-module.exports = { ShowcaseProject, Project, Score, Event, User, Email};
+module.exports = { ShowcaseProject, Project, Event, Email, User, Team };
--- app/db/schema/score.js
+++
@@ -1,9 +0,0 @@
-let Schema = require('mongoose').Schema;
-let ObjectId = Schema.ObjectId;
-
-let Score = new Schema({
-	projectNumber: { type: Number, required: true  },
-	score: { type: Number, required: true }
-});
-
-module.exports = Score;
