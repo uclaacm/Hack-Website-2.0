1164301cbeb3c130aa79c6d9465d8d431ae0ad67
Nikhil Kansal <nkansal96@gmail.com>
2016-12-25 13:30:31 -0800

    implemented get, create, join, leave team logic

--- app/hack-school/team/index.js
+++ app/hack-school/team/index.js
@@ -1,43 +1,109 @@
 const express = require('express');
+const async = require('async');
+const db = require('../../db');
 let router = express.Router();
 
+router.use((req, res, next) => {
+	if (!req.user || !req.user.id)
+		return res.status(401).json({ success: false, error: "Unauthorized" });
+	next();
+});
+
 router.get('/', (req, res) => {
-	/**
-	 * TODO: implement user's team information
-	 * Should return a team object, null if no team
-	 * If user is in a team, then the team id, name, and member profiles, total score, individual project scores
-	 */
+	if (!req.user.teamId)
+		return res.json({ success: true, error: null, team: null });
+
+	db.Team.findById(req.user.teamId, (err, team) => {
+		res.json({
+			success: !err,
+			error: err ? err : null,
+			team: err ? {} : team.getPublic()
+		});
+	});
 });
 
 router.post('/create', (req, res) => {
-	/**
-	 * TODO: implement creating a new team
-	 * If the user is already in a team, do nothing, return an error
-	 * If the user is not in a team:
-	 *   Create a new team with given req.body.name, add the current user to it
-	 *   Set current user's team ID to this team
-	 *   Return the newly created team
-	 */
+	if (req.user.teamId && req.user.teamId !== "")
+		return res.json({ success: false, error: "You are already in a team." });
+	if (!req.body.team || !req.body.team.name)
+		return res.json({ success: false, error: "Malformed request." });
+	if (!req.body.team.name.length)
+		return res.json({ success: false, error: "Invalid team name." });
+	
+	db.Team.findByName(req.body.team.name, (err, team) => {
+		if (team)
+			return res.json({ success: false, error: "A team with that name already exists." });
+		
+		let newTeam = new db.Team({
+			name: req.body.team.name,
+			members: [req.user],
+			scores: []
+		});
+
+		newTeam.save((err, updatedTeam) => {
+			if (!err) {
+				req.user.teamId = updatedTeam.id;
+				req.user.save();
+			}
+
+			res.json({
+				success: !err,
+				error: err ? err : null,
+				team: err ? {} : updatedTeam.getPublic()
+			});
+		});
+	});
 });
 
 router.get('/leave', (req, res) => {
-	/**
-	 * TODO: implement leaving the team
-	 * If the user is not in a team, do nothing, return an error
-	 * Remove the user from the team
-	 *   If the team has no members now, delete the team
-	 * Set the user's team ID to null
-	 */
+	if (!req.user.teamId)
+		return res.json({ success: false, error: "You are not in a team." });
+
+	db.Team.findById(req.user.teamId, (err, team) => {
+		if (err || !team)
+			return res.status(500).json({ success: false, error: "Could not find user team." });
+		for (let i = 0; i < team.members.length; i++) {
+			if (team.members[i].id === req.user.id) {
+				team.members.splice(i--, 1);
+			}
+		}
+
+		let teamAction = team.members.length === 0 ? team.remove : team.save;
+		teamAction(err => {
+			if (!err) {
+				req.user.teamId = "";
+				req.user.save();
+			}
+
+			res.json({
+				success: !err,
+				error: err ? err : null
+			});
+		});
+	});
 });
 
 router.post('/join', (req, res) => {
-	/**
-	 * TODO: implement joining a team
-	 * If the user is already in team, do nothing, return an error
-	 * If the team id in req.body.teamId does not exist, do nothing, return an error
-	 * Add this user to the the team given by req.body.teamId
-	 * Set this user's teamId to req.body.teamId
-	 */
+	if (req.user.teamId)
+		return res.json({ success: false, error: "You are already in a team." });
+	if (!req.body.team || !req.body.team.id)
+		return res.json({ success: false, error: "Malformed request." });
+	
+	db.Team.findById(req.body.team.id, (err, team) => {
+		if (err || !team)
+			return res.json({ success: false, error: "No team with id '" + req.body.team.id + "' exists.", team: null });
+		
+		req.user.teamId = team.id;
+		req.user.save();
+		team.members.push(req.user);
+		team.save((err, newTeam) => {
+			res.json({
+				success: !err,
+				error: err ? err : null,
+				team: err ? {} : newTeam.getPublic()
+			});
+		});
+	});
 });
 
 module.exports = { router };
