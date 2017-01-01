7a8e48b0aa0a34f73cb5f7914c6202180df0337e
Rohan Varma <rvarm1@gmail.com>
2017-01-01 01:31:56 -0800

    Revert "remove need for token to POST"
    This reverts commit 51077622f561354a3c32394dbf19e7ab21e2d75e.

--- app/api/v1/email/index.js
+++ app/api/v1/email/index.js
@@ -9,15 +9,11 @@ router.route('/:email?')
 .all((req, res, next) => {
 	req.validToken = req.body && req.body.token && crypto.verifyToken(req.body.token);
 	req.validToken = req.validToken || (req.query && req.query.token && crypto.verifyToken(req.query.token));
-	//uncomment the below code to require a token for all email related requests
-	//if (!req.validToken)
-	//	return res.status(401).json({ success: false, error: "Unauthorized" });
+	if (!req.validToken)
+		return res.status(401).json({ success: false, error: "Unauthorized" });
 	next();
 })
 .get((req, res, next) => {
-	if(!req.validToken){
-		return res.status(401).json({success: false, error: "Unauthorized request"});
-	}
 	Email.getAll((err, users) => {
 		res.json({
 			error: err ? err : null,
