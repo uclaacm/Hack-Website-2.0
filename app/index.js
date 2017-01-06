538837b9a378ab3a2f771af6b3bbcaac795ab1b4
Nikhil Kansal <nkansal96@gmail.com>
2017-01-05 19:37:31 -0800

    added concurrency

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ module.exports = {
 	api: require('./api'),
 	auth: require('./auth'),
 	ghost: require('./ghost'),
+	config: require('./config'),
 	session: require('./session'),
 	hackschool: require('./hack-school')
 };
