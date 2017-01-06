dde6825b2bcf854a938be844e073a77520a373ec
Nikhil Kansal <nkansal96@gmail.com>
2017-01-05 17:13:06 -0800

    fixed bug: overwrite environment variables with dev values

--- app/config/index.js
+++ app/config/index.js
@@ -16,14 +16,14 @@ if (env === "production") {
     config.session.secret = "77ea260f6918c0d8c3b6c35514d3b1a4fc69f01adbf7d2412611de97c3f0f2dc";
     config.database.uri = "mongodb://127.0.0.1:27017/acm-hack-db";
     config.ghost.database.uri = "postgres://localhost/acm-hack-dev-db";
+	config.facebook.appId = "236380866788670";
+	config.facebook.secret = "72afc70ce3cb5f2ba13115b2516450ed";
+//	config.facebook.appId = "229762920785401";
+//	config.facebook.secret = "a6e3b6dee5c7424fd0605a6679052739";
 }
 
 config.apiSecret = "a076ef2e85154871eb365ecb4942bfd5";
 config.ghost.url = config.host + "/blog";
-config.facebook.appId = "236380866788670";
-config.facebook.secret = "72afc70ce3cb5f2ba13115b2516450ed";
-//config.facebook.appId = "229762920785401";
-//config.facebook.secret = "a6e3b6dee5c7424fd0605a6679052739";
 config.isProduction = env === "production";
 config.isDevelopment = !config.isProduction;
 
