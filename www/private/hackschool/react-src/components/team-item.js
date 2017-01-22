6b27eb318db9704bb8f2fe66d7a3c36ead82449a
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-01-22 14:34:05 -0800

    renamed files

---
+++ www/private/hackschool/react-src/components/team-item.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class TeamItem extends Component{
+
+	constructor(props){
+		super(props);
+
+		this.formatMembers = this.formatMembers.bind(this);
+	}
+
+	formatMembers(members){
+		return members.map(member => {
+			return (
+				<li key={member.id}
+					className="team-profile-pic"
+					style={{backgroundImage: `url(${member.profilePicture.small})`}}>
+				</li>
+			);
+		});
+	}
+
+	render(){
+		return (
+			<tr className="team-item">
+				<td>
+					{this.props.rank}
+				</td>
+				<td>
+					<ul>{this.formatMembers(this.props.members)}</ul>
+				</td>
+				<td>
+					<p>{this.props.name}</p>
+				</td>
+				<td>
+					<p>{this.props.score}</p>
+				</td>
+			</tr>
+		);
+	}
+
+}
+
+export default TeamItem;
