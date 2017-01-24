a6020101879a56c747e38eac7c27cfb7349a9bd7
Nikhil Kansal <nkansal96@gmail.com>
2017-01-24 02:28:21 -0800

    removed project model from db

--- app/db/index.js
+++ app/db/index.js
@@ -6,11 +6,10 @@ mongoose.Promise = require('bluebird');
 mongoose.connect(config.database.uri);
 
 let ShowcaseProject = mongoose.model('ShowcaseProject', require('./schema/showcaseProject'));
-let Project = mongoose.model('Project', require('./schema/project'));
 let Session = mongoose.model('Session', require('./schema/session'));
 let Event = mongoose.model('Event', require('./schema/event'));
 let Email = mongoose.model('Email', require('./schema/email'));
 let User = mongoose.model('User', require('./schema/user'));
 let Team = mongoose.model('Team', require('./schema/team'));
 
-module.exports = { ShowcaseProject, Project, Session, Event, Email, User, Team };
+module.exports = { ShowcaseProject, Session, Event, Email, User, Team };
