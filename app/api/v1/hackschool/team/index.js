10ea574726005b15d695c1a8da4fa932d8f1693a
Nikhil Kansal <nkansal96@gmail.com>
2017-01-24 03:23:27 -0800

    promisified hackschool APIs

--- app/api/v1/hackschool/team/index.js
+++ app/api/v1/hackschool/team/index.js
@@ -3,5 +3,4 @@ let router = express.Router();
 
 // Route each Team API
 router.use('/score', require('./score').router);
-router.use('/attendance', require('./attendance').router);
 module.exports = { router };
